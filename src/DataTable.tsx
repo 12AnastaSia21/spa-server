f106b75bbd648be051b9ffdcec664c60ce11ff1b
Anastasia <maly1204n@gmail.com>
2024-10-02 15:13:11 +0300

    Requests for adding, changing, and deleting data

--- src/DataTable.tsx
+++ src/DataTable.tsx
@@ -31,24 +31,69 @@ interface EditToolbarProps {
   setRowModesModel: (
     newModel: (oldModel: GridRowModesModel) => GridRowModesModel
   ) => any;
+  authToken: string;
 }
 
+//Добавление новой записи в таблицу
 function EditToolbar(props: EditToolbarProps) {
-  const { setRows, setRowModesModel } = props;
+  const { setRows, setRowModesModel, authToken } = props;
 
   const handleClick = () => {
-    const id = randomId();
-    setRows((oldRows) => [...oldRows, { id, companySigDate: "", isNew: true }]);
-    setRowModesModel((oldModel) => ({
-      ...oldModel,
-      [id]: { mode: GridRowModes.Edit, fieldToFocus: "companySigDate" },
-    }));
+    const tempId = randomId();
+    const newRow = {
+      id: tempId,
+      companySigDate: new Date().toISOString(),
+      companySignatureName: "Название компании",
+      documentName: "Название документа",
+      documentStatus: "Статус",
+      documentType: "Тип документа",
+      employeeNumber: "Номер сотрудника",
+      employeeSigDate: new Date().toISOString(),
+      employeeSignatureName: "Подпись",
+    };
+
+    setRows((oldRows) => [...oldRows, newRow]);
+
+    axios
+      .post(
+        "https://test.v5.pryaniky.com/ru/data/v3/testmethods/docs/userdocs/create",
+        newRow,
+        {
+          headers: {
+            "x-auth": authToken,
+          },
+        }
+      )
+      .then((response) => {
+        const realId = response.data.data.id;
+        console.log("Запись успешно создана:", response.data);
+
+        setRows((oldRows) =>
+          oldRows.map((row) =>
+            row.id === tempId ? { ...row, id: realId, isNew: false } : row
+          )
+        );
+
+        setRowModesModel((oldModel) => ({
+          ...oldModel,
+          [realId]: {
+            mode: GridRowModes.Edit,
+            fieldToFocus: "companySigDate",
+          },
+        }));
+      })
+      .catch((error) => {
+        console.error("Ошибка при создании записи:", error);
+
+        // В случае ошибки удаляем временную строку
+        setRows((oldRows) => oldRows.filter((row) => row.id !== tempId));
+      });
   };
 
   return (
     <GridToolbarContainer>
       <Button color="primary" startIcon={<AddIcon />} onClick={handleClick}>
-        Добавит запись
+        Добавить запись
       </Button>
     </GridToolbarContainer>
   );
@@ -61,6 +106,7 @@ export default function DataTable() {
   );
   const { authToken } = useAuth();
 
+  //запрос на получение данных
   useEffect(() => {
     if (!authToken) {
       console.error("No auth token found!");
@@ -97,14 +143,49 @@ export default function DataTable() {
     setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.Edit } });
   };
 
+  //Сохранение измененных данных
   const handleSaveClick = (id: GridRowId) => () => {
-    setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.View } });
+    const rowToUpdate = rows.find((row) => row.id === id);
+    if (!rowToUpdate) return;
+
+    processRowUpdate(rowToUpdate)
+      .then((updatedRow) => {
+        setRowModesModel({
+          ...rowModesModel,
+          [id]: { mode: GridRowModes.View },
+        });
+        setRows((oldRows) =>
+          oldRows.map((row) => (row.id === id ? updatedRow : row))
+        );
+      })
+      .catch((error) => {
+        console.error("Error updating record:", error);
+      });
   };
 
+  //Запрос на удаление данных из таблицы
   const handleDeleteClick = (id: GridRowId) => () => {
     setRows(rows.filter((row) => row.id !== id));
+    axios
+      .post(
+        `https://test.v5.pryaniky.com/ru/data/v3/testmethods/docs/userdocs/delete/${id}`,
+        undefined,
+        {
+          headers: {
+            "x-auth": authToken,
+          },
+        }
+      )
+      .then((response) => {
+        console.log("Record deleted:", response.status);
+        setRows((oldRows) => oldRows.filter((row) => row.id !== id));
+      })
+      .catch((error) => {
+        console.log("Error deleting record:", error);
+      });
   };
 
+  //Отмена изменения данных
   const handleCancelClick = (id: GridRowId) => () => {
     setRowModesModel({
       ...rowModesModel,
@@ -117,10 +198,29 @@ export default function DataTable() {
     }
   };
 
+  //Запрос на изменение данных
   const processRowUpdate = (newRow: GridRowModel) => {
-    const updatedRow = { ...newRow, isNew: false };
-    setRows(rows.map((row) => (row.id === newRow.id ? updatedRow : row)));
-    return updatedRow;
+    return axios
+      .post(
+        `https://test.v5.pryaniky.com/ru/data/v3/testmethods/docs/userdocs/set/${newRow.id}`,
+        { ...newRow, isNew: false },
+        {
+          headers: {
+            "x-auth": authToken,
+          },
+        }
+      )
+      .then((response) => {
+        console.log("Record updated:", response.status);
+        return { ...newRow, isNew: false };
+      })
+      .catch((error) => {
+        console.error(
+          "Error updating record:",
+          error.response ? error.response.data : error.message
+        );
+        throw error;
+      });
   };
 
   const handleRowModesModelChange = (newRowModesModel: GridRowModesModel) => {
@@ -201,9 +301,6 @@ export default function DataTable() {
       type: "actions",
       headerName: "Действие",
       flex: 4,
-      align: "left",
-      headerAlign: "left",
-      cellClassName: "actions",
       getActions: ({ id }) => {
         const isInEditMode = rowModesModel[id]?.mode === GridRowModes.Edit;
 
@@ -218,7 +315,6 @@ export default function DataTable() {
             <GridActionsCellItem
               icon={<CancelIcon />}
               label="Cancel"
-              className="textPrimary"
               onClick={handleCancelClick(id)}
               color="inherit"
             />,
@@ -229,7 +325,6 @@ export default function DataTable() {
           <GridActionsCellItem
             icon={<EditIcon />}
             label="Edit"
-            className="textPrimary"
             onClick={handleEditClick(id)}
             color="inherit"
           />,
@@ -253,9 +348,6 @@ export default function DataTable() {
       }}
     >
       <DataGrid
-        initialState={{ pagination: { paginationModel: { pageSize: 15 } } }}
-        pagination
-        pageSizeOptions={[10, 15, 20, 50, 100]}
         rows={rows}
         columns={columns}
         editMode="row"
@@ -264,7 +356,7 @@ export default function DataTable() {
         onRowEditStop={handleRowEditStop}
         processRowUpdate={processRowUpdate}
         slots={{ toolbar: EditToolbar as GridSlots["toolbar"] }}
-        slotProps={{ toolbar: { setRows, setRowModesModel } }}
+        slotProps={{ toolbar: { setRows, setRowModesModel, authToken } }}
       />
     </Box>
   );
